8f3e23de0835ffe6c392321bfe0759523313dc66
Tonyjlum <tonyjlum@gmail.com>
2019-10-22 14:25:09 -0400

    preheroku 2.0

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module FullstackAssessmentSingleRepo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.serve_static_assets = true
-    config.assets.precompile = ['*.js', '*.css']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
